fc9bb916ac7aa1c55ef87287bbbcc557c24d294e
UwuStudio <sashadorozhkin890@gmail.com>
2023-09-06 15:29:52 +0200

    06.09.23

--- Assets/scripts/Tree.cs
+++ Assets/scripts/Tree.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
+    
+    //start
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
         
     }
+    //aktualizacja
     void Update()
     {
-        
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy")
+        {
+            Color color = spriteRenderer.color;
+            color.a = 0.5f;
+            spriteRenderer.color = color;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy")
+        {
+            Color color = spriteRenderer.color;
+            color.a = 0.5f;
+            spriteRenderer.color = color;
+        }
+
+
+
     }
 }
